frelon/scx

commit 151b1994caadeeb19a6fbae81c340144d05d4f7e

scxtop: better naming for kernel worker threads

tools/scxtop/src/lib.rs
@@ -50,6 +50,7 @@ pub use plain::Plain;
 unsafe impl Plain for crate::bpf_skel::types::bpf_event {}
 
 use smartstring::alias::String as SsoString;
+use std::ffi::CStr;
 
 pub const APP: &str = "scxtop";
 pub const TRACE_FILE_PREFIX: &str = "scxtop_trace";
@@ -365,13 +366,10 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_SCHED_WAKEUP => {
                 let wakeup = unsafe { event.event.wakeup };
-                let comm = unsafe {
-                    std::str::from_utf8(std::slice::from_raw_parts(
-                        event.event.wakeup.comm.as_ptr() as *const u8,
-                        16,
-                    ))
-                    .unwrap()
-                };
+                let comm = unsafe { CStr::from_ptr(event.event.wakeup.comm.as_ptr()) }
+                    .to_string_lossy()
+                    .to_string();
+
                 Ok(Action::SchedWakeup(SchedWakeupAction {
                     ts: event.ts,
                     cpu: event.cpu,
@@ -384,10 +382,9 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_SCHED_WAKING => {
                 let waking = unsafe { &event.event.waking };
-                let comm = std::str::from_utf8(unsafe {
-                    std::slice::from_raw_parts(waking.comm.as_ptr() as *const u8, 16)
-                })
-                .unwrap();
+                let comm = unsafe { CStr::from_ptr(waking.comm.as_ptr()) }
+                    .to_string_lossy()
+                    .to_string();
 
                 Ok(Action::SchedWaking(SchedWakingAction {
                     ts: event.ts,
@@ -406,13 +403,10 @@ impl TryFrom<&bpf_event> for Action {
             })),
             bpf_intf::event_type_EXIT => {
                 let exit = unsafe { &event.event.exit };
-                let comm = unsafe {
-                    std::str::from_utf8(std::slice::from_raw_parts(
-                        exit.comm.as_ptr() as *const u8,
-                        exit.comm.len(),
-                    ))
-                    .unwrap()
-                };
+                let comm = unsafe { CStr::from_ptr(exit.comm.as_ptr()) }
+                    .to_string_lossy()
+                    .to_string();
+
                 Ok(Action::Exit(ExitAction {
                     ts: event.ts,
                     cpu: event.cpu,
@@ -425,20 +419,13 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_FORK => {
                 let fork = unsafe { &event.event.fork };
-                let parent_comm = unsafe {
-                    std::str::from_utf8(std::slice::from_raw_parts(
-                        fork.parent_comm.as_ptr() as *const u8,
-                        fork.parent_comm.len(),
-                    ))
-                    .unwrap()
-                };
-                let child_comm = unsafe {
-                    std::str::from_utf8(std::slice::from_raw_parts(
-                        fork.child_comm.as_ptr() as *const u8,
-                        fork.child_comm.len(),
-                    ))
-                    .unwrap()
-                };
+                let parent_comm = unsafe { CStr::from_ptr(fork.parent_comm.as_ptr()) }
+                    .to_string_lossy()
+                    .to_string();
+                let child_comm = unsafe { CStr::from_ptr(fork.child_comm.as_ptr()) }
+                    .to_string_lossy()
+                    .to_string();
+
                 Ok(Action::Fork(ForkAction {
                     ts: event.ts,
                     cpu: event.cpu,
@@ -451,20 +438,12 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_SCHED_SWITCH => {
                 let sched_switch = unsafe { &event.event.sched_switch };
-                let prev_comm = unsafe {
-                    std::str::from_utf8(std::slice::from_raw_parts(
-                        sched_switch.prev_comm.as_ptr() as *const u8,
-                        sched_switch.prev_comm.len(),
-                    ))
-                    .unwrap()
-                };
-                let next_comm = unsafe {
-                    std::str::from_utf8(std::slice::from_raw_parts(
-                        sched_switch.next_comm.as_ptr() as *const u8,
-                        sched_switch.next_comm.len(),
-                    ))
-                    .unwrap()
-                };
+                let prev_comm = unsafe { CStr::from_ptr(sched_switch.prev_comm.as_ptr()) }
+                    .to_string_lossy()
+                    .to_string();
+                let next_comm = unsafe { CStr::from_ptr(sched_switch.next_comm.as_ptr()) }
+                    .to_string_lossy()
+                    .to_string();
 
                 Ok(Action::SchedSwitch(SchedSwitchAction {
                     ts: event.ts,

tools/scxtop/src/bpf/main.bpf.c
@@ -405,6 +405,23 @@ int BPF_KPROBE(scx_dispatch_from_dsq_set_vtime, struct bpf_iter_scx_dsq *it__ite
 	return on_move_set_vtime(NULL, vtime);
 }
 
+static void record_real_comm(char *comm, struct task_struct *task)
+{
+	if (task->flags & PF_WQ_WORKER) {
+		/*
+		 * Worker queue thread names are 32 characters long but let's
+		 * stick with the measly 16 characters of the comm field to
+		 * keep things simple.
+		 */
+		struct kthread *k = bpf_core_cast(task->worker_private,
+						  struct kthread);
+		struct worker *worker = bpf_core_cast(k->data, struct worker);
+		bpf_probe_read_kernel_str(comm, MAX_COMM, worker->desc);
+	} else {
+		__builtin_memcpy_inline(comm, &task->comm, MAX_COMM);
+	}
+}
+
 static __always_inline int __on_sched_wakeup(struct task_struct *p)
 {
 	struct task_ctx *tctx;
@@ -429,7 +446,8 @@ static __always_inline int __on_sched_wakeup(struct task_struct *p)
 	event->event.wakeup.pid = p->pid;
 	event->event.wakeup.tgid = p->tgid;
 	event->event.wakeup.prio = (int)p->prio;
-	__builtin_memcpy_inline(&event->event.wakeup.comm, &p->comm, MAX_COMM);
+	record_real_comm(event->event.wakeup.comm, p);
+
 	bpf_ringbuf_submit(event, 0);
 
 	return 0;
@@ -464,7 +482,7 @@ int BPF_PROG(on_sched_waking, struct task_struct *p)
 	event->event.wakeup.pid = p->pid;
 	event->event.wakeup.tgid = p->tgid;
 	event->event.wakeup.prio = (int)p->prio;
-	__builtin_memcpy(&event->event.wakeup.comm, &p->comm, MAX_COMM);
+	record_real_comm(event->event.wakeup.comm, p);
 
 	bpf_ringbuf_submit(event, 0);
 	return 0;
@@ -524,7 +542,7 @@ int BPF_PROG(on_sched_switch, bool preempt, struct task_struct *prev,
 			event->event.sched_switch.next_dsq_nr = 0;
 			event->event.sched_switch.next_dsq_vtime = 0;
 		}
-		__builtin_memcpy(&event->event.sched_switch.next_comm, &next->comm, MAX_COMM);
+		record_real_comm(event->event.sched_switch.next_comm, next);
 	} else {
 		event->event.sched_switch.next_pid = 0;
 		event->event.sched_switch.next_tgid = 0;
@@ -542,7 +560,7 @@ int BPF_PROG(on_sched_switch, bool preempt, struct task_struct *prev,
 		} else {
 			event->event.sched_switch.prev_dsq_id = SCX_DSQ_INVALID;
 		}
-		__builtin_memcpy(&event->event.sched_switch.prev_comm, &prev->comm, MAX_COMM);
+		record_real_comm(event->event.sched_switch.prev_comm, prev);
 	} else {
 		event->event.sched_switch.prev_pid = 0;
 		event->event.sched_switch.prev_tgid = 0;
@@ -803,7 +821,7 @@ int BPF_PROG(on_sched_exit, struct task_struct *task)
 	event->event.exit.pid = BPF_CORE_READ(task, pid);
 	event->event.exit.tgid = BPF_CORE_READ(task, tgid);
 	event->event.exit.prio = BPF_CORE_READ(task, prio);
-	__builtin_memcpy_inline(&event->event.exit.comm, &task->comm, MAX_COMM);
+	record_real_comm(event->event.exit.comm, task);
 
 	bpf_ringbuf_submit(event, 0);
 
@@ -826,8 +844,8 @@ int BPF_PROG(on_sched_fork, struct task_struct *parent, struct task_struct *chil
 	event->ts = bpf_ktime_get_ns();
 	event->event.fork.parent_pid = BPF_CORE_READ(parent, pid);
 	event->event.fork.child_pid = BPF_CORE_READ(child, pid);
-	__builtin_memcpy_inline(&event->event.fork.parent_comm, &parent->comm, MAX_COMM);
-	__builtin_memcpy_inline(&event->event.fork.child_comm, &child->comm, MAX_COMM);
+	record_real_comm(event->event.fork.parent_comm, parent);
+	record_real_comm(event->event.fork.child_comm, child);
 
 	bpf_ringbuf_submit(event, 0);
 

commit ae8ed72737f4cb41cb4eaf4396dbf0106ab2cc4c

scxtop: fix for building on aarch64+riscv64
Commit 151b1994caad ("scxtop: better naming for kernel worker threads")
introduces some code that does not compile for aarch64/riscv64:

```
error[E0308]: mismatched types
   --> tools/scxtop/src/lib.rs:473:57
    |
473 |                 let next_comm = unsafe { CStr::from_ptr(sched_switch.next_comm.as_ptr()) }
    |                                          -------------- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ expected `*const u8`, found `*const i8`
    |                                          |
    |                                          arguments to this function are incorrect
    |
    = note: expected raw pointer `*const u8`
               found raw pointer `*const i8`
```

It seems the bindgen is outputting [i8; 16] for next_comm which means
as_ptr returns a *const i8. Casting this to the platform dependant
c_char (which translates to u8/i8 based on the platform) makes the code
build on all platforms.

Fixes: 151b1994caad ("scxtop: better naming for kernel worker threads")
Signed-off-by: Fredrik Lönnegren <fredrik@frelon.se>

tools/scxtop/src/lib.rs
@@ -40,6 +40,7 @@ pub use perfetto_trace::PerfettoTraceManager;
 pub use protos::*;
 pub use stats::StatAggregation;
 pub use stats::VecStats;
+pub use std::ffi::c_char;
 pub use theme::AppTheme;
 pub use tui::Event;
 pub use tui::Tui;
@@ -390,9 +391,10 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_SCHED_WAKEUP => {
                 let wakeup = unsafe { event.event.wakeup };
-                let comm = unsafe { CStr::from_ptr(event.event.wakeup.comm.as_ptr()) }
-                    .to_string_lossy()
-                    .to_string();
+                let comm =
+                    unsafe { CStr::from_ptr(event.event.wakeup.comm.as_ptr() as *const c_char) }
+                        .to_string_lossy()
+                        .to_string();
 
                 Ok(Action::SchedWakeup(SchedWakeupAction {
                     ts: event.ts,
@@ -406,7 +408,7 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_SCHED_WAKING => {
                 let waking = unsafe { &event.event.waking };
-                let comm = unsafe { CStr::from_ptr(waking.comm.as_ptr()) }
+                let comm = unsafe { CStr::from_ptr(waking.comm.as_ptr() as *const c_char) }
                     .to_string_lossy()
                     .to_string();
 
@@ -427,7 +429,7 @@ impl TryFrom<&bpf_event> for Action {
             })),
             bpf_intf::event_type_EXIT => {
                 let exit = unsafe { &event.event.exit };
-                let comm = unsafe { CStr::from_ptr(exit.comm.as_ptr()) }
+                let comm = unsafe { CStr::from_ptr(exit.comm.as_ptr() as *const c_char) }
                     .to_string_lossy()
                     .to_string();
 
@@ -443,12 +445,14 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_FORK => {
                 let fork = unsafe { &event.event.fork };
-                let parent_comm = unsafe { CStr::from_ptr(fork.parent_comm.as_ptr()) }
-                    .to_string_lossy()
-                    .to_string();
-                let child_comm = unsafe { CStr::from_ptr(fork.child_comm.as_ptr()) }
-                    .to_string_lossy()
-                    .to_string();
+                let parent_comm =
+                    unsafe { CStr::from_ptr(fork.parent_comm.as_ptr() as *const c_char) }
+                        .to_string_lossy()
+                        .to_string();
+                let child_comm =
+                    unsafe { CStr::from_ptr(fork.child_comm.as_ptr() as *const c_char) }
+                        .to_string_lossy()
+                        .to_string();
 
                 Ok(Action::Fork(ForkAction {
                     ts: event.ts,
@@ -467,12 +471,14 @@ impl TryFrom<&bpf_event> for Action {
             #[allow(non_upper_case_globals)]
             bpf_intf::event_type_SCHED_SWITCH => {
                 let sched_switch = unsafe { &event.event.sched_switch };
-                let prev_comm = unsafe { CStr::from_ptr(sched_switch.prev_comm.as_ptr()) }
-                    .to_string_lossy()
-                    .to_string();
-                let next_comm = unsafe { CStr::from_ptr(sched_switch.next_comm.as_ptr()) }
-                    .to_string_lossy()
-                    .to_string();
+                let prev_comm =
+                    unsafe { CStr::from_ptr(sched_switch.prev_comm.as_ptr() as *const c_char) }
+                        .to_string_lossy()
+                        .to_string();
+                let next_comm =
+                    unsafe { CStr::from_ptr(sched_switch.next_comm.as_ptr() as *const c_char) }
+                        .to_string_lossy()
+                        .to_string();
 
                 Ok(Action::SchedSwitch(SchedSwitchAction {
                     ts: event.ts,